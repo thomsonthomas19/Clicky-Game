dcb787e29bfb7c3891a21b0ebaa3a8700b9d9132
t2million <thomsonthomas19@gmail.com>
2019-01-15 20:53:36 -0500

    Updates

---
+++ precache-manifest.f5ee25f741af104695f667718c13d886.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6acb6692e4ee25941da6",
+    "url": "/clicky-game/static/js/runtime~main.6acb6692.js"
+  },
+  {
+    "revision": "8252ab5c929fdd47d7a1",
+    "url": "/clicky-game/static/js/main.8252ab5c.chunk.js"
+  },
+  {
+    "revision": "9c49b1657f794805e3dd",
+    "url": "/clicky-game/static/js/1.9c49b165.chunk.js"
+  },
+  {
+    "revision": "8252ab5c929fdd47d7a1",
+    "url": "/clicky-game/static/css/main.ba767986.chunk.css"
+  },
+  {
+    "revision": "a6667dbabfd87918aa1014262d7e61e8",
+    "url": "/clicky-game/index.html"
+  }
+];
